f67ec82ace3e43d6796a6193d27dfe029369aed4
Najeeb Arif <NajeebArif786@outlook.com>
2022-11-26 17:59:33 +0530

    Add entity objects

---
+++ src/main/java/narif/poc/netclickdataapi/NetclickDataApiApplication.java
@@ -0,0 +1,13 @@
+package narif.poc.netclickdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetclickDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetclickDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/narif/poc/netclickdataapi/NetclickDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package narif.poc.netclickdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetclickDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
